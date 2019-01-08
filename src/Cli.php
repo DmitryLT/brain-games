44b3a05c5b94328dfdf31532c75075f9410f55f8
DmitryLT <dim.sha@yandex.ru>
2019-01-08 14:00:18 +0300

    Добавлен phpcs в require-dev, lint в Makefile, исправлены ошибки codesniffer'а

--- src/Cli.php
+++ src/Cli.php
@@ -1,9 +1,21 @@
 <?php
+/**
+ * Файл для описания функций
+ * PHP version 7.2.0
+ * @category Executable_File
+ * @package  TaggartBrain-games
+ * @author  DmitryLT <dim.sha@yandex.ru>
+ * @license  no license
+ * @link  https://packagist.org/packages/taggart/brain-games
+ * */
 
 namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
-
+/**
+ * Функция инициализации
+ * @return null
+ */
 function run()
 {
     line('Welcome to the Brain Game!');
